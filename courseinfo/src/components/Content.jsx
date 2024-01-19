447652ec5f980829bfc918b5eb0b2f6c270c2d94
Sean <seanhoyeonjin@gmail.com>
2024-01-19 12:54:15 -0500

    finished exercise 1.4

--- courseinfo/src/components/Content.jsx
+++ courseinfo/src/components/Content.jsx
@@ -5,7 +5,13 @@ function Content(props) {
   return (
     <>
       <p>
-        {props.part.name} {props.part.exercises}
+        {props.parts[0].name} {props.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts[1].name} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].name} {props.parts[2].exercises}
       </p>
     </>
   );
