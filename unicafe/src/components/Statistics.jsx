d86b8535d3b902acb0e67be18528a333cd1ff36c
Sean <seanhoyeonjin@gmail.com>
2024-01-22 10:52:27 -0500

    finished excercise 1.7

--- unicafe/src/components/Statistics.jsx
+++ unicafe/src/components/Statistics.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 function Statistics({ good, neutral, bad }) {
+  const positive = (good / (good + neutral + bad)) * 100;
+
+  const score = good * 1 + bad * -1;
+  const total = good + neutral + bad;
+  const average = score / total;
+
   return (
     <>
       <h1>Statistics</h1>
       <p>good - {good}</p>
       <p>neutral - {neutral}</p>
       <p>bad - {bad}</p>
+      <p>all - {good + neutral + bad}</p>
+      <p>average - {average}</p>
+      <p>positive - {positive} %</p>
     </>
   );
 }
